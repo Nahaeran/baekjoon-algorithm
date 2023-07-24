4123c001da95f7067411fc2174320a04417b38f3
Nahaeran <nh2545@naver.com>
2023-07-24 23:35:52 +0900

    2447 별 찍기-10

---
+++ 별_찍기/2447_별_찍기-10.py
@@ -0,0 +1,27 @@
+n = int(input())
+
+
+def fractal_rectangle(num):
+    if num == 1:
+        return ["*"]
+
+    result = []
+    lst = fractal_rectangle(num // 3)
+
+    # 첫 번째 줄
+    for e in lst:
+        result.append(e * 3)
+    # 두 번째 줄
+    for e in lst:
+        result.append(e + (" " * (num // 3)) + e)
+    # 세 번째 줄
+    for e in lst:
+        result.append(e * 3)
+
+    return result
+
+
+# 출력
+rectangle = fractal_rectangle(n)
+for i in range(n):
+    print(rectangle[i])
