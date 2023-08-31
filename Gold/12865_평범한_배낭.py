eed05f372d3bc3692f2b2e4c394aefe13afeb32f
nahaeran <nh2545@naver.com>
2023-08-31 13:52:01 +0900

    solved: 12865 평범한 배낭

---
+++ Gold/12865_평범한_배낭.py
@@ -0,0 +1,24 @@
+import sys
+
+
+def knapsack(W, wt, val, n):
+    K = [[0 for x in range(W+1)] for x in range(n+1)]
+    for i in range(n+1):
+        for w in range(W+1):
+            if i==0 or w==0:
+                K[i][w] = 0
+            elif wt[i-1] <= w:
+                K[i][w] = max(val[i-1]+K[i-1][w-wt[i-1]], K[i-1][w])
+            else:
+                K[i][w] = K[i-1][w]
+    return K[n][W]
+
+
+wt = []
+val = []
+N, K = map(int, sys.stdin.readline().strip().split())
+for i in range(N):
+    w, v = map(int, sys.stdin.readline().strip().split())
+    wt.append(w)
+    val.append(v)
+print(knapsack(K, wt, val, N))
