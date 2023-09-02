2137d023effefa9d457860fc286ea9ab2c2307e9
Nahaeran <nh2545@naver.com>
2023-09-02 22:53:12 +0900

    solved: 17281 ⚾

--- Gold/17281_⚾.py
+++ Gold/17281_⚾.py
@@ -1,74 +1,53 @@
-def play_inning(player, now_idx, score):
-    out = 0
-    queue = [0, 0, 0]
-    while out < 3:
-        if player[now_idx] == 4:
-            score += sum(queue) + 1
-            queue = [0, 0, 0]
-        elif player[now_idx] == 3:
-            score += queue[0]
-            score += queue[1]
-            score += queue[2]
-            queue = [1, 0, 0]
-        elif player[now_idx] == 2:
-            score += queue[0]
-            score += queue[1]
-            queue = [queue[2], 1, 0]
-        elif player[now_idx] == 1:
-            score += queue[0]
-            queue = [queue[1], queue[2], 1]
-        else:
-            out += 1
-        now_idx += 1
-        now_idx %= 9
-    return score, now_idx
+import sys
+
+input = sys.stdin.readline
 
 
-def get_permutations(arr, n):
-    result = []
-    if n == 0:
-        return [[]]
+def play_inning(count):
+    global result
+    if count == 9:
+        start = score = 0
+        for inning in inning_points:
+            out = 0
+            r1, r2, r3 = 0, 0, 0
+            while out < 3:
+                now_idx = order_list[start]
+                if inning[now_idx] == 4:
+                    score += r1 + r2 + r3 + 1
+                    r1, r2, r3 = 0, 0, 0
+                elif inning[now_idx] == 3:
+                    score += r1 + r2 + r3
+                    r1, r2, r3 = 0, 0, 1
+                elif inning[now_idx] == 2:
+                    score += r2 + r3
+                    r1, r2, r3 = 0, 1, r1
+                elif inning[now_idx] == 1:
+                    score += r3
+                    r1, r2, r3 = 1, r1, r2
+                else:
+                    out += 1
+                start = (start + 1) % 9
+        result = max(result, score)
+        return
 
-    for i, elem in enumerate(arr):
-        for P in get_permutations(arr[:i] + arr[i + 1:], n - 1):
-            result += [[elem] + P]
-    if n == 8:
-        for per in result:
-            per.insert(3, 0)
-    return result
+    for i in range(9):
+        if selected[i]:
+            continue
+        selected[i] = 1
+        order_list[i] = count
+        play_inning(count + 1)
+        selected[i] = 0
+        order_list[i] = 0
 
 
 N = int(input())
 inning_points = [list(map(int, input().split())) for _ in range(N)]
-score_list = []
+order_list = [0] * 9
+selected = [0] * 9
 
-permutation_list = get_permutations(list(range(1, 9)), 8)
+order_list[3] = 0
+selected[3] = 1
+result = 0
 
-
-#for inning in inning_points:
-
-
-
-point_list = []
-for permutation in permutation_list:
-    score = 0
-    now = 0
-
-    sorted_inning = []
-    for inning in inning_points:
-        point_list = [0] * 9
-        for i in range(9):
-            idx = permutation.index(i)
-            point_list[idx] = inning[i]
-        sorted_inning.append(point_list)
-
-    if sorted_inning in point_list:
-        continue
-    else:
-        point_list.append(sorted_inning)
-
-    for inning in sorted_inning:
-        score, now = play_inning(inning, now, score)
-    score_list.append(score)
-
-print(max(score_list))
+play_inning(1)
+print(result)
