086a02c1e30d9e647fa2fdcf2c4b7cebf36a0044
Hae ran <102934823+Nahaeran@users.noreply.github.com>
2023-09-30 22:36:09 +0900

    solved: 10826 피보나치 수 4

---
+++ Silver/10826_피보나치_수_4.py
@@ -0,0 +1,31 @@
+import sys
+sys.setrecursionlimit(int(1e9))
+N = int(input()) - 2
+matrix = ((1, 1), (1, 0))
+
+
+def mul_matrix(mat1, mat2):
+    result = [[0] * 2 for _ in range(2)]
+    for i in range(2):
+        for j in range(2):
+            for k in range(2):
+                result[i][j] += mat1[i][k] * mat2[k][j]
+    return result
+
+
+def fib(mat, n):
+    if n == 1:
+        return mat
+    else:
+        temp = fib(mat, n // 2)
+        if n % 2 == 0:
+            return mul_matrix(temp, temp)
+        else:
+            return mul_matrix(mul_matrix(temp, temp), mat)
+
+
+if N > 0:
+    result = sum(fib(matrix, N)[0])
+    print(result)
+else:
+    print((0, 1, 1)[N + 2])
