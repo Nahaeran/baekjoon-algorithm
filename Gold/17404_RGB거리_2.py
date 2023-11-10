4c91618238d7b415fdcbb5dcfc23c4701c3947c4
nahaeran <nh2545@naver.com>
2023-11-10 11:54:12 +0900

    solved: 17404 RGB거리 2

---
+++ Gold/17404_RGB거리_2.py
@@ -0,0 +1,18 @@
+import sys
+
+N = int(sys.stdin.readline())
+costs = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+result = INF = 1e9
+
+for i in range(3):
+    dp = [[INF, INF, INF] for _ in range(N)]
+    dp[0][i] = costs[0][i]
+    for j in range(1, N):
+        dp[j][0] = costs[j][0] + min(dp[j - 1][1], dp[j - 1][2])
+        dp[j][1] = costs[j][1] + min(dp[j - 1][0], dp[j - 1][2])
+        dp[j][2] = costs[j][2] + min(dp[j - 1][0], dp[j - 1][1])
+    for k in range(3):
+        if i != k:
+            result = min(result, dp[-1][k])
+
+print(result)
